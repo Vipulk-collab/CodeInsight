75f8c95c0907a044c5fd896156495bf71161ec82
Vipulk-collab <127003302@sastra.ac.in>
2026-07-16 23:04:36 +0530

    Complete code ingestion and retrieval pipipeline

---
+++ tests/test_index_repository.py
@@ -0,0 +1,126 @@
+import sys
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(PROJECT_ROOT))
+from ingestion.github_loader import (
+    get_python_files
+)
+
+from ingestion.github_loader import (
+    clone_repo,
+    get_python_files
+)
+
+from parser.ast_parser import (
+    parse_python_file
+)
+
+from chunking.chunk_builder import (
+    create_chunks_from_nodes
+)
+
+from shared.encoder import (
+    encode_documents
+)
+
+from shared.config import (
+    repo_slug
+)
+
+from storage.chroma_store import (
+    get_collection,
+    upsert_chunks
+)
+
+
+def index_repository(
+    repo_url: str
+):
+
+    repo_name = repo_slug(
+        repo_url
+    )
+
+    repo_path = str(
+        Path("data/repos")
+        / repo_name
+    )
+
+    print(
+        f"Cloning {repo_url}"
+    )
+
+    clone_repo(
+        repo_url,
+        repo_path
+    )
+
+    files = get_python_files(
+        repo_path
+    )
+
+    all_chunks = []
+
+    for file in files:
+
+        try:
+
+            nodes = parse_python_file(
+                str(file)
+            )
+
+            chunks = (
+                create_chunks_from_nodes(
+                    nodes,
+                    str(file),
+                    repo_path
+                )
+            )
+
+            all_chunks.extend(
+                chunks
+            )
+
+        except Exception as e:
+
+            print(
+                f"Skipping {file}"
+            )
+
+            print(e)
+
+    print(
+        f"Chunks Created: {len(all_chunks)}"
+    )
+
+    texts = [
+
+        chunk["code_text"]
+
+        for chunk in all_chunks
+    ]
+
+    embeddings = encode_documents(
+        texts
+    )
+
+    collection = get_collection(
+        repo_name
+    )
+
+    upsert_chunks(
+        collection,
+        all_chunks,
+        embeddings
+    )
+
+    print(
+        f"Stored {len(all_chunks)} chunks"
+    )
+
+    return {
+        "repo_name": repo_name,
+        "files": len(files),
+        "chunks": len(all_chunks)
+    }
